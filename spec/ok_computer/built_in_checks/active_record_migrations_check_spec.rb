8683f05e42043ebcf636ea0aa5da07bb732a9e5d
Dewey Garwood <dewey.garwood@gmail.com>
2025-01-21 14:48:55 -0700

    refactor tests

--- spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
+++ spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
@@ -2,98 +2,58 @@ require "rails_helper"
 
 module OkComputer
   describe ActiveRecordMigrationsCheck do
+    let(:rails_pending_migration_method) do
+      if Rails.version >= "7.1"
+        # rails now supports multiple databases and checks
+        # that any of them have pending migrations
+        :check_all_pending!
+      else
+        :check_pending!
+      end
+    end
+
     it "is a subclass of Check" do
       expect(subject).to be_a Check
     end
 
-    current_rails_version = Gem::Version.new(ActiveRecord::VERSION::STRING)
-
-    # 4.0 <= Rails < 7.2
-    if current_rails_version >= Gem::Version.new("7.1")
-      # rails now supports multiple databases and checks
-      # that any of them have pending migrations
-
-      context "#check" do
-        before do
-          allow(Rails.configuration.active_record).to receive(:migration_error).and_return(nil)
-        end
-
-        context "with no pending migrations" do
-          before do
-            expect(ActiveRecord::Migration).to receive(:check_all_pending!).and_return(nil)
-          end
-
-          it { is_expected.to be_successful_check }
-          it { is_expected.to have_message "NO pending migrations" }
-        end
-
-        context "with pending migrations" do
-          before do
-            expect(ActiveRecord::Migration).to receive(:check_all_pending!).and_raise(ActiveRecord::PendingMigrationError)
-          end
-
-          it { is_expected.not_to be_successful_check }
-          it { is_expected.to have_message "Pending migrations" }
-        end
-
-        context "when active_record.migration_error set to :page_load" do
-          before do
-            allow(Rails.application.config.active_record).to receive(:migration_error).and_return(:page_load)
-          end
-
-          it { is_expected.to be_successful_check }
-          it { is_expected.to have_message "NOTE: pending migrations are checked on page_load" }
-
-          context "when in the development env" do
-            before do
-              allow(Rails.env).to receive(:development?).and_return(true)
-            end
-
-            it { is_expected.to be_successful_check }
-            it { is_expected.to have_message "NO pending migrations" }
-          end
-        end
+    context "#check" do
+      before do
+        allow(Rails.configuration.active_record).to receive(:migration_error).and_return(nil)
       end
-    else # Rails < 7.1
-      context "#check" do
+
+      context "with no pending migrations" do
         before do
-          allow(Rails.configuration.active_record).to receive(:migration_error).and_return(nil)
+          expect(ActiveRecord::Migration).to receive(rails_pending_migration_method).and_return(nil)
         end
 
-        context "with no pending migrations" do
+        it { is_expected.to be_successful_check }
+        it { is_expected.to have_message "NO pending migrations" }
+      end
+
+      context "with pending migrations" do
+        before do
+          expect(ActiveRecord::Migration).to receive(rails_pending_migration_method).and_raise(ActiveRecord::PendingMigrationError)
+        end
+
+        it { is_expected.not_to be_successful_check }
+        it { is_expected.to have_message "Pending migrations" }
+      end
+
+      context "when active_record.migration_error set to :page_load" do
+        before do
+          allow(Rails.application.config.active_record).to receive(:migration_error).and_return(:page_load)
+        end
+
+        it { is_expected.to be_successful_check }
+        it { is_expected.to have_message "NOTE: pending migrations are checked on page_load" }
+
+        context "when in the development env" do
           before do
-            expect(ActiveRecord::Migration).to receive(:check_pending!).and_return(nil)
+            allow(Rails.env).to receive(:development?).and_return(true)
           end
 
           it { is_expected.to be_successful_check }
           it { is_expected.to have_message "NO pending migrations" }
-        end
-
-        context "with pending migrations" do
-          before do
-            expect(ActiveRecord::Migration).to receive(:check_pending!).and_raise(ActiveRecord::PendingMigrationError)
-          end
-
-          it { is_expected.not_to be_successful_check }
-          it { is_expected.to have_message "Pending migrations" }
-        end
-
-        context "when active_record.migration_error set to :page_load" do
-          before do
-            allow(Rails.application.config.active_record).to receive(:migration_error).and_return(:page_load)
-          end
-
-          it { is_expected.to be_successful_check }
-          it { is_expected.to have_message "NOTE: pending migrations are checked on page_load" }
-
-          context "when in the development env" do
-            before do
-              allow(Rails.env).to receive(:development?).and_return(true)
-            end
-
-            it { is_expected.to be_successful_check }
-            it { is_expected.to have_message "NO pending migrations" }
-          end
         end
       end
     end
