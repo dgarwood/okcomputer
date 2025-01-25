145bb970378a4f35af39600e154b2e58e4ef0505
Dewey Garwood <dewey.garwood@gmail.com>
2025-01-25 01:18:47 -0700

    refactor for clarity; use Gem::Version in tests again

--- lib/ok_computer/built_in_checks/active_record_migrations_check.rb
+++ lib/ok_computer/built_in_checks/active_record_migrations_check.rb
@@ -2,20 +2,11 @@ module OkComputer
   class ActiveRecordMigrationsCheck < Check
     # Public: Check if migrations are pending or not
     def check
-      # this check is only valid if config.active_record.migration_error
-      # is set to false rather than :page_load
-      # :page_load is the default in development, otherwise this is false
-      if Rails.configuration.active_record.migration_error
-        return mark_message("NO pending migrations") if Rails.env.development?
+      return mark_message(page_load_message) if check_on_page_load?
 
-        return check_on_page_load
-      end
-
-      if Gem::Version.new(Rails.version) >= Gem::Version.new("7.1")
-        ActiveRecord::Migration.check_all_pending!
-      else
-        ActiveRecord::Migration.check_pending!
-      end
+      # produces ActiveRecord::PendingMigrationError
+      # if pending migrations exist
+      check_pending(Rails.version)
 
       mark_message "NO pending migrations"
     rescue ActiveRecord::PendingMigrationError
@@ -25,10 +16,25 @@ module OkComputer
 
     private
 
-    # We do not fail the check here since this method is only called
-    # when rails is configured to make the check and throw an error
-    def check_on_page_load
-      mark_message "NOTE: pending migrations are checked on page_load"
+    # this check is only valid if config.active_record.migration_error is set to false rather than :page_load
+    # :page_load adds a Middleware check that throws an error before the call reaches ok_computer
+    # we only run this check when :page_load is set if there are no pending migrations
+    # :page_load is the default in development, otherwise this is false
+    # see https://github.com/rails/rails/blob/d39db5d1891f7509cde2efc425c9d69bbb77e670/activerecord/lib/active_record/railtie.rb#L103
+    def check_on_page_load?
+      Rails.configuration.active_record.migration_error
+    end
+
+    def check_pending(version_string)
+      if Gem::Version.new(version_string) >= Gem::Version.new("7.1")
+        ActiveRecord::Migration.check_all_pending!
+      else
+        ActiveRecord::Migration.check_pending!
+      end
+    end
+
+    def page_load_message
+      "NOTE: pending migrations are checked on page_load"
     end
   end
 end
--- spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
+++ spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 module OkComputer
   describe ActiveRecordMigrationsCheck do
     let(:rails_pending_migration_method) do
-      if Rails.version >= "7.1"
+      if Gem::Version.new(Rails.version) >= Gem::Version.new("7.1")
         # rails now supports multiple databases and checks
         # that any of them have pending migrations
         :check_all_pending!
@@ -32,7 +32,8 @@ module OkComputer
 
       context "with pending migrations" do
         before do
-          expect(ActiveRecord::Migration).to receive(rails_pending_migration_method).and_raise(ActiveRecord::PendingMigrationError)
+          expect(ActiveRecord::Migration)
+            .to receive(rails_pending_migration_method).and_raise(ActiveRecord::PendingMigrationError)
         end
 
         it { is_expected.not_to be_successful_check }
@@ -46,15 +47,6 @@ module OkComputer
 
         it { is_expected.to be_successful_check }
         it { is_expected.to have_message "NOTE: pending migrations are checked on page_load" }
-
-        context "when in the development env" do
-          before do
-            allow(Rails.env).to receive(:development?).and_return(true)
-          end
-
-          it { is_expected.to be_successful_check }
-          it { is_expected.to have_message "NO pending migrations" }
-        end
       end
     end
   end
